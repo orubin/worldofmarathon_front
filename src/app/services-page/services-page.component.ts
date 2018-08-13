b73fce92533d730d1de8c00eb215a7b8b72ce786
Or Rubin <or.rubin@hotmail.com>
2018-08-13 16:37:45 +0300

    Fix spas

--- src/app/services-page/services-page.component.ts
+++ src/app/services-page/services-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ExternalService } from '../models/external_service';
-import {ApiService} from "../services/api.service";
+import { ApiService } from "../services/api.service";
 
 @Component({
   selector: 'app-services-page',
@@ -16,51 +16,7 @@ export class ServicesPageComponent implements OnInit {
   public externalServices:Array<ExternalService>;
 
   ngOnInit() {
-    //this.availableServices = this.apiService.services;
-    // this.apiService.getExternalCityServices(10);
-
-    this.availableServices = [
-      {
-        serviceName: 'Spa',
-        serviceList: [{
-            name: 'spa A',
-            price: 400,
-            rank: 4,
-            imageLink: "../../assets/images/services/spa.jpg",
-            amountAvailable: 8
-        },{
-          name: 'spa B',
-          price: 300,
-          rank: 4,
-          imageLink: "../../assets/images/services/spa.jpg",
-          amountAvailable: 8
-        },{
-          name: 'spa C',
-          price: 200,
-          rank: 3,
-          imageLink: "../../assets/images/services/spa.jpg",
-          amountAvailable: 8
-        }
-        ]
-      },
-      {
-        serviceName: 'Restaurants',
-        serviceList: [{
-          name: 'restaurant A',
-          rank: 4,
-          imageLink: "../../assets/images/services/restarunt.jpg"
-        },{
-          name: 'restaurant B',
-          rank: 4,
-          imageLink: "../../assets/images/services/restarunt.jpg"
-        },{
-          name: 'restaurant C',
-          rank: 3,
-          imageLink: "../../assets/images/services/restarunt.jpg"
-        }
-        ]
-      }
-    ]
+    this.availableServices = this.apiService.services;
   }
 
 }
